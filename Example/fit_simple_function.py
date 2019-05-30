485a6316348b080e13d53c9521af7c1b3795058f
TommorowIsBetter <841331749@qq.com>
2019-05-30 14:12:42 +0800

    Add feed_dict in fit_simple_function.py.

--- Example/fit_simple_function.py
+++ Example/fit_simple_function.py
@@ -7,13 +7,16 @@
 """
 import numpy as np
 import tensorflow as tf
+coefficients = np.array([[1.], [-10.], [25.]])
 w = tf.Variable(0, dtype=tf.float32)
-cost = tf.add(tf.add(w ** 2.0, tf.multiply(-10.0, w)), 25.0)
+x = tf.placeholder(tf.float32, [3, 1])
+# cost = tf.add(tf.add(w ** 2.0, tf.multiply(-10.0, w)), 25.0)
 # cost = w ** 2 - 10 * w + 25
+cost = x[0][0] * w ** 2 + x[1][0] * w + x[2][0]
 train = tf.train.GradientDescentOptimizer(0.01).minimize(cost)
 init = tf.global_variables_initializer()
 session = tf.Session()
 session.run(init)
 for i in range(1000):
-    session.run(train)
+    session.run(train, feed_dict={x: coefficients})
 print(session.run(w))
